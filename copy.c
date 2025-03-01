b54ed68163e10231c2e8691e2c4d1b21931ab852
GoutteDooo <goudal.donovan@live.fr>
2025-03-01 23:05:45 +0100

    2025-03-01 21:46:01

--- copy.c
+++ copy.c
@@ -8,6 +8,10 @@ int main(void)
 {
   char *s = get_string("s: ");
   char *t = malloc(strlen(s) + 1);//gimme 4 bytes of memo
+  if (t == NULL)
+  {
+    return 1;
+  }
 
   for (int i = 0, n = strlen(s); i <= n; i++)
   {
