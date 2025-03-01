7cabedbe2a2a9e62578c4925f380f51cc2ce73ce
GoutteDooo <goudal.donovan@live.fr>
2025-03-01 22:53:40 +0100

    2025-03-01 21:46:01

--- copy.c
+++ copy.c
@@ -1,12 +1,19 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(void)
 {
   char *s = get_string("s: ");
-  char *t = s;
+  char *t = malloc(strlen(s) + 1);//gimme 4 bytes of memo
+
+  for (int i = 0, n = strlen(s); i <= n; i++)
+  {
+    t[i] = s[i];
+  }
+
 
   t[0] = toupper(t[0]);
 
