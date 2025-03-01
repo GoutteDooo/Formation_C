e69e287e15ebcebae4688a07c5a4e5354be35873
GoutteDooo <goudal.donovan@live.fr>
2025-03-01 23:36:50 +0100

    $(date '+%Y-%m-%d')

--- commit.sh
+++ commit.sh
@@ -7,12 +7,13 @@ while true; do
     # Vérifier s'il y a des fichiers modifiés ou en attente d'ajout
     if [[ -n $(git status --porcelain) ]]; then
         echo "Des modifications détectées, commit en cours..."
-        
+        commit_message="$(date '+%Y-%m-%d %H:%M:%S')"
+
         # Ajouter les fichiers modifiés
         git aa
 
         # Commiter avec la date actuelle
-        git datecommit
+        git commit -m "$commit_message"
 
         # (Optionnel) Pousser automatiquement les commits
         # git push origin main  # ou la branche sur laquelle tu travailles
