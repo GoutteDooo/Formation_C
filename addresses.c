fa65b982f777c632bbba2d5dbe0f5fb8d8452ebd
GoutteDooo <goudal.donovan@live.fr>
2025-03-01 22:00:38 +0100

    2025-03-01 21:46:01

--- addresses.c
+++ addresses.c
@@ -1,16 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 //test
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
+    if (argc != 3)
     {
         printf("error\n");
         return 1;
     }
-    int i = get_string("i: ");
-    int j = get_string("j: ");
-    if (strcmp(i,j) == 0)
+    if (strcmp(argv[1],argv[2]) == 0)
     {
         printf("same\n");
     }
@@ -18,5 +17,6 @@ int main(int argc, char* argv[])
     {
         printf("diff\n");
     }
+    printf("locs : %p, %p\n",argv[1], argv[2]);
     return 0;
 }
