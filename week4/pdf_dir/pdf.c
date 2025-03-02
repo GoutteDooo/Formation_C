09211ded1e942358309e06c77990f5d40931dd44
GoutteDooo <goudal.donovan@live.fr>
2025-03-02 23:20:41 +0100

    2025-03-02 23:20:41

--- week4/pdf_dir/pdf.c
+++ week4/pdf_dir/pdf.c
@@ -8,9 +8,13 @@ typedef uint8_t BYTE;
 int main(int argc, char* argv[])
 {
   if (argc != 2) return 1;
-  FILE* input = fopen("hi.txt", "rb");
+  FILE *input = fopen("hi.txt", "rb");
   BYTE b;
-  char* s = fread(&b, 1, 4, input);
+  while(fread(&b, sizeof(b), 4, input))
+  {
+
+  }
   printf("%s\n",s);
+  fclose(input);
   return 0;
 }
