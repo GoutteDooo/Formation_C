f25376429d0fe487a231c627f2c5e511bf886a9b
GoutteDooo <goudal.donovan@live.fr>
2025-03-02 23:35:41 +0100

    2025-03-02 23:35:41

--- week4/pdf_dir/pdf.c
+++ week4/pdf_dir/pdf.c
@@ -9,11 +9,18 @@ int main(int argc, char* argv[])
   if (argc != 2) return 1;
   FILE* input = fopen(argv[1], "r");
 
-  uint8_t b[4];//size of byte : unsigned int (8B)
+  uint8_t buffer[4];//size of byte : unsigned int (8B)
 
-  uint8_t signature[] = {0x25, 0X50, 0x44, 0x46}
+  uint8_t signature[] = {0x25, 0X50, 0x44, 0x46};
 
-  fread(buffer, sizeof())
+  fread(buffer, sizeof(uint8_t), 4, input);
+
+  if (buffer == signature)
+  {
+    printf("Success!\n");
+    return 0;
+  }
+  printf("Nope.\n");
 
   fclose(input);
   return 0;
