c00e40180d040ee6c9a8e118736d9c2d1a532a00
GoutteDooo <goudal.donovan@live.fr>
2025-03-02 22:42:10 +0100

    2025-03-02 22:42:10

--- pdf.c
+++ pdf.c
@@ -1,1 +1,17 @@
-inc
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+  char* file = argv[1];
+  int r_queue = strlen(file) - 5;
+  char* pdf_buffer = malloc(4);
+  for (int i = strlen(file) - 1; i > r_queue; i--)
+  {
+    *pdf_buffer += file[i];
+    printf("%c\n",file[i]);
+  }
+  return 0;
+}
