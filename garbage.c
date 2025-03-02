357052b707a69326c5dee186181065855639681e
GoutteDooo <goudal.donovan@live.fr>
2025-03-02 12:00:23 +0100

    2025-03-02 12:00:23

--- garbage.c
+++ garbage.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <cs50.h>
 
 int main(void)
 {
-  int scores[1024];
-  for (int i = 0; i < 1024; i++)
-  {
-    printf("%i\n", scores[i]);
-  }
+  int *x;
+  // int *y;
+
+  x = malloc(sizeof(int));
+
+  *x = 42;
+
+  printf("x = %i\n",*x);
   return 0;
 }
