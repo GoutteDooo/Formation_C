0bd14b3cd5ab97bbacafe6c4294278db95044f5b
GoutteDooo <goudal.donovan@live.fr>
2025-03-02 23:17:41 +0100

    2025-03-02 23:17:41

--- week4/pdf_dir/pdf.c
+++ week4/pdf_dir/pdf.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+typedef uint8_t BYTE;
 
 int main(int argc, char* argv[])
 {
   if (argc != 2) return 1;
-  FILE* input = fopen("hi.txt");
+  FILE* input = fopen("hi.txt", "rb");
   BYTE b;
-  fread(&b, 1, 4, input);
+  char* s = fread(&b, 1, 4, input);
+  printf("%s\n",s);
   return 0;
 }
