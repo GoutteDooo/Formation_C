3aa78e94a2133882e9c96a3dc8760bca52a1f69c
GoutteDooo <goudal.donovan@live.fr>
2025-03-02 12:18:23 +0100

    2025-03-02 12:18:23

---
+++ swap.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+void swap(int a, int b);
+
+int main(void)
+{
+  int a = 12;
+  int b = 33;
+
+  swap(a,b);
+
+  printf("a: %i\n",a);
+  printf("b: %i\n",b);
+  return 0;
+}
+
+void swap(int a, int b)
+{
+  int tmp = a;
+  a = b;
+  b = tmp;
+}
