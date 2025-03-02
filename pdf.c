a42a33f3b9a3de280fadb67ffb63ff8683211a60
GoutteDooo <goudal.donovan@live.fr>
2025-03-02 22:57:10 +0100

    2025-03-02 22:57:10

--- pdf.c
+++ pdf.c
@@ -5,21 +5,6 @@
 int main(int argc, char* argv[])
 {
   if (argc != 2) return 1;
-  char* file = argv[1];
-  int r_queue = strlen(file) - 5;
-  char* pdf_buffer = malloc(4);
-  for (int i = strlen(file) - 1, j = i; i > r_queue; i--, j -= 2)
-  {
-    pdf_buffer[i - j] = file[i];
-  }
-  if (strcmp(pdf_buffer, "fdp.") == 0) {
-    printf("yes\n");
-    return 0;
-  }
-  else
-  {
-    printf("no\n");
-    return 1;
-  }
+  
   return 0;
 }
