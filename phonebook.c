847a30493e81261319bd804f77896d212841829a
GoutteDooo <goudal.donovan@live.fr>
2025-03-02 17:18:59 +0100

    2025-03-02 17:18:59

--- phonebook.c
+++ phonebook.c
@@ -5,6 +5,11 @@
 int main(void)
 {
   FILE *file = fopen("phonebook.csv", "a");//w = write, a = append
+  if (file == NULL)
+  {
+    return 1;
+  }
+
   char *name = get_string("Name: ");
   char *number = get_string("Number: ");
 
