65b08058330a302d8bed525993299edca3fa0783
GoutteDooo <goudal.donovan@live.fr>
2025-03-03 18:47:15 +0100

    2025-03-03 18:47:15

--- week4/pointers/cp.c
+++ week4/pointers/cp.c
@@ -2,13 +2,17 @@
 
 int main(int argc, char* argv[])
 {
-  if (argc != 2) return 1;
+  if (argc != 3) return 1;
   FILE* input = fopen(argv[1],"r");
   FILE* output = fopen(argv[2],"w");
   char ch;
-  while ((ch = fgetc(argv[1])) != EOF)
+  while ((ch = fgetc(input)) != EOF)
   {
-    fputc(ch, argv[2]);
+    printf("%c",ch);
+    fputc(ch, output);
   }
+  fclose(input);
+  fclose(output);
+  printf("succes.\n");
   return 0;
 }
