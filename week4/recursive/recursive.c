74c301197ab29d0c223ac5491444cadd1ed5d9f5
GoutteDooo <goudal.donovan@live.fr>
2025-03-03 16:03:55 +0100

    2025-03-03 16:03:55

--- week4/recursive/recursive.c
+++ week4/recursive/recursive.c
@@ -1,9 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
+void recursive(int c);
 
 int main(void)
 {
-  
+  recursive(3);
   return 0;
 }
 
@@ -11,9 +12,13 @@ void recursive(int c)
 {
   if (c == 0)
   {
-    return 0;
+    return;
   }
   recursive(c - 1);
+  //Here, the function stops, and put another recursive on top of the stack
+  //Then, the other recursives functions who hasn't c == 0 will follows the same rhythm.
+  //And, when c reaches 0
+  //The stack will pop off one by one recursives function with c from 0 to the default one.
   for (int i = 0; i < c; i++)
   {
     printf("#");
