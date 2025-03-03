376df519525ea97bc9fb74ec3823696a093c7fb4
GoutteDooo <goudal.donovan@live.fr>
2025-03-03 11:13:02 +0100

    2025-03-03 11:13:02

--- week4/pointers/pointers.c
+++ week4/pointers/pointers.c
@@ -6,15 +6,13 @@ int main(void)
 {
   int k = 52;
   int* pk = &k;
-  // printf("k before dec: %i\n", k);
+  printf("k before dec: %i\n", k);
   decrement(pk);
-  // printf("k after dec: %i\n", k);
+  printf("k after dec: %i\n", k);
   return 0;
 }
 
 void decrement(int *x)
 {
-  printf("x before dec pointed: %p\n", x);
-  x--;
-  printf("x after dec pointed: %p\n", x);
+  (*x)--;
 }
