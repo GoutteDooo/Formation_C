c296d262f52cec3cad3e50f7612ef772532c6a04
GoutteDooo <goudal.donovan@live.fr>
2025-03-03 16:06:55 +0100

    2025-03-03 16:06:55

--- week4/recursive/recursive.c
+++ week4/recursive/recursive.c
@@ -19,6 +19,15 @@ void recursive(int c)
   //Then, the other recursives functions who hasn't c == 0 will follows the same rhythm.
   //And, when c reaches 0
   //The stack will pop off one by one recursives function with c from 0 to the default one.
+  /**
+  Example :
+  recursive(3)
+  Stack when c reaches 0:
+  recursive(0) <-- step 1 : return !
+  recursive(1) <-- step 2 : #
+  recursive(2) <-- step 3 : ##
+  recursive(3) <-- step 4 : ###
+   */
   for (int i = 0; i < c; i++)
   {
     printf("#");
