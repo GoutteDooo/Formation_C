62d89395e0c2cd8e8cb58602aba94cb3c89052f8
GoutteDooo <goudal.donovan@live.fr>
2025-03-03 11:22:02 +0100

    2025-03-03 11:22:02

--- week4/pointers/pointers.c
+++ week4/pointers/pointers.c
@@ -1,18 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
-void decrement(int *x);
+void pdecrement(int *x);
+int vdecrement(int x);
 
 int main(void)
 {
   int k = 52;
   int* pk = &k;
   printf("k before dec: %i\n", k);
-  decrement(pk);
-  printf("k after dec: %i\n", k);
+  pdecrement(pk);
+  printf("k after pdec: %i\n", k);
+  k = vdecrement(k);
+  printf("k after vdec: %i\n", k);
   return 0;
 }
 
-void decrement(int *x)
+void pdecrement(int *x)
 {
   (*x)--;
+}
+
+int vdecrement(int x)
+{
+  return --x;
 }
