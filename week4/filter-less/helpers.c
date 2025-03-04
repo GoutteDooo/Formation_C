3dd5cc57f0ace65b359a603b4ba3a7bade7b10b9
GoutteDooo <goudal.donovan@live.fr>
2025-03-04 10:01:06 +0100

    2025-03-04 10:01:06

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -33,10 +33,10 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             //Pour chaque pixel
-            double sepia_red = image[i][j].rgbtRed * 0.393;
-            double green = image[i][j].rgbtGreen;
-            double blue = image[i][j].rgbtBlue;
-            double avg = (red + green + blue) / 3;
+            BYTE red = image[i][j].rgbtRed;
+            BYTE green = image[i][j].rgbtGreen;
+            BYTE blue = image[i][j].rgbtBlue;
+            BYTE avg = (red + green + blue) / 3;
             // printf("avg= %f\n",avg);
             //Puis, ajouter cette moyenne dans chacune des couleurs pour donner un niveau de gris
             image[i][j].rgbtRed = avg;
