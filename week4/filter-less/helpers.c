4bf7550713eb22269a80297afc94c56781476e54
GoutteDooo <goudal.donovan@live.fr>
2025-03-04 09:58:06 +0100

    2025-03-04 09:58:06

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -27,6 +27,23 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    //Parcourir tout les pixels de l'image
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            //Pour chaque pixel
+            double sepia_red = image[i][j].rgbtRed * 0.393;
+            double green = image[i][j].rgbtGreen;
+            double blue = image[i][j].rgbtBlue;
+            double avg = (red + green + blue) / 3;
+            // printf("avg= %f\n",avg);
+            //Puis, ajouter cette moyenne dans chacune des couleurs pour donner un niveau de gris
+            image[i][j].rgbtRed = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtBlue = avg;
+        }
+    }
     return;
 }
 
