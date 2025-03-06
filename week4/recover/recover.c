3f9efcd7df724941336236ab50411fddc1eb2196
GoutteDooo <goudal.donovan@live.fr>
2025-03-06 19:41:50 +0100

    2025-03-06 19:41:50

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -19,13 +19,17 @@ int main(int argc, char *argv[])
   }
   //Rechercher par paquets de 512 B (stockés dans un buffer)
   uint8_t search[512];
+  int count = 0;
   //Une fois le fichier ouvert, le parcourir et trouver une signature JPEG (ffd8ffe?) -> 4 first B of block of 512 B
   while (fread(search, 512, 1, card))
   {
     //Une fois une signature trouvée, créer un fichier avec pour nom "###.jpg" (démarrer a 000.jpg et incrémenter à chaque trouvaille)
-    if (search[0] == 0xff && search[1] == 0xd8 && search[2] == 0xff)
+    if (search[0] == 0xff && search[1] == 0xd8 && search[2] == 0xff && (search[3] & 0xF0) == 0xE0)
     {
-      printf("JPEG maybe found!");
+      //Signature JPEG trouvée
+      count++;
+      printf("Jpeg found : %i\n", count);
+      
     }
   }
   //Remplir le nouveau fichier jusqu'à trouver une prochaine signature JPEG ou arriver à la fin de la lecture
