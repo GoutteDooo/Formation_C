ee58ffcdc7d72bf66b08b78c3653b04d82f34242
GoutteDooo <goudal.donovan@live.fr>
2025-03-06 19:14:50 +0100

    2025-03-06 19:14:50

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <sys/types.h>
 typedef __int8_t BYTE;
 
 int main(int argc, char *argv[])
@@ -16,13 +17,15 @@ int main(int argc, char *argv[])
       printf("Could not open %s.\n", infile);
       return 1;
   }
-  //Une fois le fichier ouvert, le parcourir et trouver une signature JPEG
+  //Une fois le fichier ouvert, le parcourir et trouver une signature JPEG (ffd8ffe?)
   //Une fois une signature trouvée, créer un fichier avec pour nom "###.jpg" (démarrer a 000.jpg et incrémenter à chaque trouvaille)
   //
-  BYTE b;
+  __int16_t b;
   while ((b = fgetc(inptr)) != EOF)
   {
-    printf("%x",b);
+    if (b == "ff") {
+      printf("%x",b);
+    }
   }
   printf("\n");
   fclose(inptr);
