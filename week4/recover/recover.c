7c037fb81a079fc00b5fa98e55dc1492f72fe14a
GoutteDooo <goudal.donovan@live.fr>
2025-03-06 16:10:03 +0100

    2025-03-06 16:10:03

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+  FILE* card = fopen("card.raw", "r");
+  fprintf(card,"r");
+  int v = 2;
+  printf("ok %i\n", v);
+  fclose(card);
 }
