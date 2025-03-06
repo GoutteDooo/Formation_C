bf5be88bcff0caf457fdcd596f5fedfade03345f
GoutteDooo <goudal.donovan@live.fr>
2025-03-06 19:23:50 +0100

    2025-03-06 19:23:50

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -18,9 +18,9 @@ int main(int argc, char *argv[])
       return 1;
   }
   //Rechercher par paquets de 512 B
-  //Une fois le fichier ouvert, le parcourir et trouver une signature JPEG (ffd8ffe?)
+  //Une fois le fichier ouvert, le parcourir et trouver une signature JPEG (ffd8ffe?) -> 4 first B of block of 512 B
   //Une fois une signature trouvée, créer un fichier avec pour nom "###.jpg" (démarrer a 000.jpg et incrémenter à chaque trouvaille)
-  //
+  //Remplir le nouveau fichier jusqu'à trouver une prochaine signature JPEG ou arriver à la fin de la lecture
   __int16_t b;
   while ((b = fgetc(inptr)) != EOF)
   {
