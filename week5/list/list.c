56f0b4e23f1ce83ef584320316b218985fd9ed62
GoutteDooo <goudal.donovan@live.fr>
2025-03-07 10:27:20 +0100

    2025-03-07 10:27:20

--- week5/list/list.c
+++ week5/list/list.c
@@ -1,42 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct node
+{
+  int number;
+  struct node* next;
+} node;
+
 int main(void)
 {
-  int* list = malloc(3 * sizeof(int));
-  if (list == NULL)
+  node* list = NULL;
+
+  for (int i = 0; i < 3; i++)
   {
-    return 1;
+    
   }
-  list[0] = 1;
-  list[1] = 2;
-  list[2] = 3;
-
-  //Time passes
-
-  int *tmp = realloc(list, 4 * sizeof(int));
-  if (tmp == NULL)
-  {
-    free(list);
-    return 1;
-  }
-
-  tmp[3] = 4;
-  for (int i = 0; i < 4; i++)
-  {
-    printf("%i\n", tmp[i]);
-  }
-
-  free(list);
-  /*
-  list = tmp;
-
-for (int i = 0; i < 4; i++)
-{
-  printf("%i\n", list[i]);
-}
-
-// Time passes
-free(list);
-*/
   return 0;
 }
