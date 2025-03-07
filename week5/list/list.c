2200f36a647c94afa5ef92f2a219f846b04027bd
GoutteDooo <goudal.donovan@live.fr>
2025-03-07 18:05:16 +0100

    2025-03-07 18:05:16

--- week5/list/list.c
+++ week5/list/list.c
@@ -11,16 +11,28 @@ typedef struct node
 int main(void)
 {
   node* list = NULL;
-
-  for (int i = 0; i < 3; i++)
+  int count = get_int("Number: ");
+  for (int i = 0; i < count; i++)
   {
     node* n = malloc(sizeof(node));
     if (n == NULL)
     {
       return 1;
     }
-    n->number = get_int("Number: ");
-    n->next = NULL;
+    n->number = i + 1;
+    n->next = list;
+    
+    list = n;
   }
+
+  //Time passes
+  node *ptr = list;
+
+  while (ptr != NULL)
+  {
+    printf("%i\n", ptr->number);
+    ptr = ptr->next;
+  }
+
   return 0;
 }
