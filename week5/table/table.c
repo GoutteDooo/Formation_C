4aa322c841c091f491e6fef44a3fbd96ae0a2b7f
GoutteDooo <goudal.donovan@live.fr>
2025-03-10 21:37:54 +0100

    2025-03-10 21:37:54

---
+++ week5/table/table.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct node
+{
+  string phrase;
+  struct node* next;
+} node;
+
+node* table[26];
+
+int hash(string phrase);
+bool unload(node* list);
+void visualizer(node* list);
+
+int main(void)
+{
+  // Add items
+  for (int i = 0; i < 3; i++)
+  {
+    string phrase = get_string("Enter a new phrase: ");
+
+    //Find phrase bucket
+    int bucket = hash(phrase);
+    printf("%s hashes to %i\n", phrase, bucket);
+  }
+
+  return 0;
+}
+//TODO: return the correct bucket for a given phrase
