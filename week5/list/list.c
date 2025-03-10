643cc79df53a4c36af09dc4a62a55ee95dae1882
GoutteDooo <goudal.donovan@live.fr>
2025-03-10 20:49:54 +0100

    2025-03-10 20:49:54

--- week5/list/list.c
+++ week5/list/list.c
@@ -2,14 +2,37 @@
 #include <stdlib.h>
 #include <cs50.h>
 
+
 typedef struct node
 {
-  int number;
+  string phrase;
   struct node* next;
 } node;
 
+#define LIST_SIZE = 2
+
+bool unload(node * list);
+void visualizer(node *list);
+
 int main(void)
 {
-  
+  node* list = NULL;
+
+  for (int i = 0; i < LIST_SIZE; i++)
+  {
+
+  }
+  string phrase = get_string("Enter a phrase: ");
+  node *n = malloc(sizeof(node));
+  if (n == NULL)
+  {
+    return 1;
+  }
+
+  n->phrase = phrase;
+  n->next = NULL;
+  n->next = list;
+
+  visualizer(list);
   return 0;
 }
