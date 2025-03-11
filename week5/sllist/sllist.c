8482b22e30c9f426407d719dd55a8b9263dfac12
GoutteDooo <goudal.donovan@live.fr>
2025-03-11 12:46:24 +0100

    2025-03-11 12:46:24

--- week5/sllist/sllist.c
+++ week5/sllist/sllist.c
@@ -5,36 +5,53 @@
 typedef struct slnode
 {
   char* phrase;
+  int id;
   struct slnode* next;
 } slnode;
 
-slnode* create(slnode* list, char* phrase);
+slnode* create(slnode* list, char* phrase, int id);
 void print_list(slnode* list);
 void free_list(slnode* list);
+void destroy(slnode* list, int id);
 
 int main(void)
 {
   slnode* list = malloc(sizeof(slnode));
   if (list == NULL) return 1;
   list->phrase = "Hello!";
+  list->id = 0;
   list->next = NULL;
   for (int i = 0; i < 3; i++)
   {
     string s = get_string("Enter a phrase: ");
-    slnode* node = create(list, s);
+    slnode* node = create(list, s, i+1);
     list = node;
   }
   print_list(list);
-  bool 
+  bool finish = false;
+  while (!finish)
+  { 
+    int d = get_int("Enter id to destroy: ");
+    if (d) {
+      destroy(list, d);
+    }
+    else
+    {
+      break;
+    }
+    printf("new list :\n");
+    print_list(list);
+  }
   free_list(list);
   return 0;
 }
 
-slnode* create(slnode* list, char* phrase)
+slnode* create(slnode* list, char* phrase, int id)
 {
   slnode* new_node = malloc(sizeof(slnode));
   if (new_node == NULL) return NULL;
   new_node->phrase = phrase;
+  new_node->id = id;
   new_node->next = list;
   return new_node;
 }
@@ -58,4 +75,9 @@ void free_list(slnode* list)
     free(ptr);
     ptr = list;
   }
+}
+
+void destroy(slnode* list, int id)
+{
+  
 }
