b6ef8b9e6db1a6bce701c6b7596ccc6b36cb3a04
GoutteDooo <goudal.donovan@live.fr>
2025-03-11 12:13:24 +0100

    2025-03-11 12:13:24

--- week5/sllist/sllist.c
+++ week5/sllist/sllist.c
@@ -7,13 +7,26 @@ typedef struct slnode
   struct slnode* next;
 } slnode;
 
+slnode* create(slnode* list, char* phrase);
+
 int main(void)
 {
   slnode* list = malloc(sizeof(slnode));
   if (list == NULL) return 1;
   list->phrase = "Hello!";
   list->next = NULL;
+  slnode* node = create(list, "saloute");
   printf("phrase: %s, next: %p\n", list->phrase, list->next);
 
   return 0;
+}
+
+slnode* create(slnode* list, char* phrase)
+{
+  slnode* new_node = malloc(sizeof(slnode));
+  if (new_node == NULL) return NULL;
+  new_node->phrase = phrase;
+  new_node->next = list;
+  list = new_node;
+  return new_node;
 }
