337c1c5953c157273b3a5de090156a60ba771800
GoutteDooo <goudal.donovan@live.fr>
2025-03-11 17:11:34 +0100

    2025-03-11 17:11:34

--- week5/dllist/dllist.c
+++ week5/dllist/dllist.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cs50.h>
 
 typedef struct dllnode
 {
   char* phrase;
   int id;
-  struct dllnode* next;
   struct dllnode* before;
+  struct dllnode* next;
 } dllnode;
+#define NODES_NUMBER 3
 
 int main(void)
 {
   dllnode* list = malloc(sizeof(dllnode));
-  
+  string phrase = get_string("Enter a phrase: ");
+  list->phrase = phrase;
+  list->id = 0;
+  list->before = NULL;
+  list->next = NULL;
+  for (int i = 0; i < NODES_NUMBER; i++)
+  {
+    
+    string phrase = get_string("Enter a phrase: ");
+
+  }
+
   return 0;
 }
