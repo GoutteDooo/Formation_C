9044c9ba140d184427c72847203b276b8d7c71c6
GoutteDooo <goudal.donovan@live.fr>
2025-03-11 17:14:34 +0100

    2025-03-11 17:14:34

--- week5/dllist/dllist.c
+++ week5/dllist/dllist.c
@@ -9,7 +9,11 @@ typedef struct dllnode
   struct dllnode* before;
   struct dllnode* next;
 } dllnode;
+
 #define NODES_NUMBER 3
+
+dllnode* create_node(dllnode* list);
+
 
 int main(void)
 {
@@ -21,10 +25,21 @@ int main(void)
   list->next = NULL;
   for (int i = 0; i < NODES_NUMBER; i++)
   {
-    
+    list->next = create_node(list);
     string phrase = get_string("Enter a phrase: ");
 
   }
 
   return 0;
+}
+
+dllnode* create_node(dllnode* list)
+{
+  dllnode* new_node = malloc(sizeof(dllnode));
+  list->next = new_node;
+  string s = get_string("Enter a phrase: ");
+  new_node->phrase = s;
+  new_node->before = list;
+  new_node->next = NULL;
+  return new_node;
 }
