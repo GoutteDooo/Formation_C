37c4dddb58b426edaf25bdaca148a5cb16a090cb
GoutteDooo <goudal.donovan@live.fr>
2025-03-18 11:48:44 +0100

    2025-03-18 11:48:44

--- Python/week6/edge.py
+++ Python/week6/edge.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageFilter
 
 before = Image.open("image.bmp")
-after = before.filter(ImageFilter.BoxBlur(1))
+after = before.filter(ImageFilter.FIND_EDGES)
 after.save("out.bmp")
