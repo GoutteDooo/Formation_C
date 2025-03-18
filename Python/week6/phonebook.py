a250d99d253fc904070acad9f74b91f720fb339b
GoutteDooo <goudal.donovan@live.fr>
2025-03-18 22:48:50 +0100

    2025-03-18 22:48:50

--- Python/week6/phonebook.py
+++ Python/week6/phonebook.py
@@ -1,3 +1,4 @@
+"""
 names = ["Yuliia","David","John"]
 
 name = input("Name: ")
@@ -6,3 +7,19 @@ if name in names:
   print("Found!")
 else:
   print(":(... Not found.")
+"""
+
+people = [
+  {
+    "name": "Yuliia",
+    "number": "+1165546875",
+  },
+  {
+    "name": "David",
+    "number": "+1486547564",
+  },
+  {
+    "name": "John",
+    "number": "+1687654782",
+  }
+]
