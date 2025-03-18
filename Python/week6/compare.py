bc9bd76418933b6e71c3fe73d00ec2806d1c5a1e
GoutteDooo <goudal.donovan@live.fr>
2025-03-18 17:28:23 +0100

    2025-03-18 17:28:23

--- Python/week6/compare.py
+++ Python/week6/compare.py
@@ -1,7 +1,17 @@
+x = int(input("x: "))
+y = int(input("y: "))
 
 if x > y:
-  print("x is more than y")
+  print("x is greater than y")
 elif (x < y):
-  print("y is more than x")
+  print("x is less than y")
 else:
   print("x is equal to y")
+
+s = input("s:")
+t = input("t: ")
+
+if s == t:
+  print("Same")
+else:
+  print("Different")
