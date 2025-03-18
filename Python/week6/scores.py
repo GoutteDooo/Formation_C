036eea101102ea0461c83375af6766c7580b0a29
GoutteDooo <goudal.donovan@live.fr>
2025-03-18 22:36:50 +0100

    2025-03-18 22:36:50

--- Python/week6/scores.py
+++ Python/week6/scores.py
@@ -1,5 +1,12 @@
-scores = [72, 73, 33]
+from cs50 import get_int
+
+scores = []
+
+for i in range(3):
+  score = get_int("Score: ")
+  scores += [score]
 
 average = sum(scores)/len(scores)
 
-print(f"{average:.50}")
+print(scores)
+print(average)
