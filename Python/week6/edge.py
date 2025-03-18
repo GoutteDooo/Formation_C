a35a7a28cd135e567bbc9cb9ae121360625ef3d5
GoutteDooo <goudal.donovan@live.fr>
2025-03-18 13:35:22 +0100

    2025-03-18 13:35:22

--- Python/week6/edge.py
+++ Python/week6/edge.py
@@ -1,5 +1,4 @@
 from PIL import Image, ImageFilter
-import cs50
 
 before = Image.open("image.bmp")
 after = before.filter(ImageFilter.FIND_EDGES)
---
+++ Python/week6/get.py
@@ -0,0 +1,5 @@
+import cs50
+
+answer = cs50.get_string("What's your name ? ")
+age = cs50.get_int("How old are you ? ")
+print(f"Hello, {answer}, you have {age} y.o.")
