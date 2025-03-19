a9b9fbd9c6020169e664777f0d721395ff80f756
GoutteDooo <goudal.donovan@live.fr>
2025-03-19 12:53:11 +0100

    2025-03-19 12:53:11

--- Python/week6/loop.py
+++ Python/week6/loop.py
@@ -1,2 +1,8 @@
-for i in range(ord('a'),ord('z'),1):
+for i in range(ord('a'),ord('z'),5):
   print(i)
+
+phrase = "You're off to Great Places"
+
+for i in range(len(phrase)):
+  print(phrase[i], end="/")
+print()
