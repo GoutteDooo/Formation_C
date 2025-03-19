58367fa31b4ca80a3988ff94735fe2956cf08931
GoutteDooo <goudal.donovan@live.fr>
2025-03-19 13:17:11 +0100

    2025-03-19 13:17:11

--- Python/week6/song.py
+++ Python/week6/song.py
@@ -1,6 +1,9 @@
-song = {"name": "Perfect", "tempo": 95.05}
-song["album"] = "Divide"
-song["instrument1"] = "Guitar"
+song = {
+  "music1": {"name": "Perfect", "tempo": 95.05},
+  "music2": {"name": "Eastside", "tempo": 88.10},
+  "music3": {"name": "Wolves", "tempo": 124.55},
+  "music4": {"name": "Him & I", "tempo": 87.35},
+  }
 
-print(song["instrument1"])
-print(f"song: {song}")
+print(song["music2"]["name"])
+# print(f"song: {song}")
