b2abb2cbe1f5998a26a30119059885d634040b8d
GoutteDooo <goudal.donovan@live.fr>
2025-03-19 12:41:43 +0100

    2025-03-19 12:41:43

---
+++ Python/week6/phrase.py
@@ -0,0 +1,10 @@
+phrase = "You're off to Great Places !"
+
+
+
+print(phrase.lower())
+print(phrase.capitalize())
+print(phrase.split())
+print(phrase.isspace())
+print(phrase.strip())
+print(phrase.upper())
