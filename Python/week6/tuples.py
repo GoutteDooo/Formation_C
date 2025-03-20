a1ca95c2030275344b1f018810e49f06781e6d36
GoutteDooo <goudal.donovan@live.fr>
2025-03-20 10:48:24 +0100

    2025-03-20 10:48:24

---
+++ Python/week6/tuples.py
@@ -0,0 +1,16 @@
+presidents = [
+  ("George Washington", "1789-1797"),
+  ("John Adams", "1797-1801"),
+  ("Thomas Jefferson", "1801-1809"),
+  ("James Madison", "1809-1817"),
+  ("James Monroe", "1817-1825"),
+  ("John Quincy Adams", "1825-1829"),
+  ("Andrew Jackson", "1829-1837"),
+  ("Martin Van Buren", "1837-1841"),
+  ("William Henry Harrison", "1841-1841"),
+  ("John Tyler", "1841-1845"),
+  ("James K. Polk", "1845-1849"), 
+]
+
+for prez, year in presidents:
+  print(f"In year {year}, {prez} was president.")
