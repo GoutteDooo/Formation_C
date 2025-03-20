bcb63d78e59548a10bc9dd6c5407c82130a70249
GoutteDooo <goudal.donovan@live.fr>
2025-03-20 12:19:56 +0100

    2025-03-20 12:19:56

--- Python/week6/sentimental-credit.py
+++ Python/week6/sentimental-credit.py
@@ -4,18 +4,8 @@ n = get_int("Number: ")
 
 # Luhn's Algorithm
 def luhn(n):
-  sum = 0
-  length = len(str(n))
-  for i in range(length, 0, -2):
-    n = int(n / 10)
-    digit = n % 10
-    digit *= 2
-    sum += digit
-    n = int(n / 10)
-    print("n: ", n)
-    print("digit: ", digit)
-    print("sum: ", sum)
-  return sum % 10 == 0
+  numbers = [int(x) for x in str(n)]
+  print(numbers)
 
 if luhn(n):
   print("Valid")
