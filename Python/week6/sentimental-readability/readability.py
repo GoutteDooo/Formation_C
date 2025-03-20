f8f679b5f09fce44924facd4b550c01083d97824
GoutteDooo <goudal.donovan@live.fr>
2025-03-20 13:26:57 +0100

    2025-03-20 13:26:57

--- Python/week6/sentimental-readability/readability.py
+++ Python/week6/sentimental-readability/readability.py
@@ -14,10 +14,12 @@ L = 100 * sum_letters / len(words)
 # counts number of sentences
 sum_words = (len(words))
 # get rid of blank items
-sum_sentences = len(text.split("."))
+sum_sentences = len([phrase for phrase in text.split(".") if phrase])
 S = 100 * (sum_sentences) / (sum_words)
 
 ColemanLiauIndex = ((0.0588 * L) - (0.296 * S) - 15.8)
+
+print(S, L)
 
 if ColemanLiauIndex < 0:
   print("Before Grade 1")
