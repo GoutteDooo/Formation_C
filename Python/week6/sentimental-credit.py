f1128743fcee9c23acd73dec0c6b078129c45437
GoutteDooo <goudal.donovan@live.fr>
2025-03-20 12:13:56 +0100

    2025-03-20 12:13:56

--- Python/week6/sentimental-credit.py
+++ Python/week6/sentimental-credit.py
@@ -8,11 +8,12 @@ def luhn(n):
   for i in range(16, 0, -1):
     n = n / 10
     digit = n % 10
-    if i % 2 == 1:
-      digit *= 2
-    if digit > 9:
-      digit -= 9
+    digit *= 2
     sum += digit
+    n = n / 10
+    print("n: ", n)
+    print("digit: ", digit)
+    print("sum: ", sum)
   return sum % 10 == 0
 
 if luhn(n):
