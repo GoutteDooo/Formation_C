95ce6233ed631f9405702161a4bb134d7aa3c175
GoutteDooo <goudal.donovan@live.fr>
2025-03-20 13:05:57 +0100

    2025-03-20 13:05:57

--- Python/week6/sentimental-readability/readability.py
+++ Python/week6/sentimental-readability/readability.py
@@ -7,14 +7,15 @@ text = inpute
 # counts number of words
 words = text.split()
 words = [''.join(char for char in word if char.isalpha()) for word in words]
-sum_letters = sum(len(word) for word in words) / len(words)
+sum_letters = sum(len(word) for word in words)
 L = sum_letters
 
 # counts number of characters
 
 
 # counts number of sentences
-sum_sentences = (len(text.split(".")) - 1)
 sum_words = (len(words))
+sum_sentences = (len(text.split(".")) - 1)
+S = 100 * sum_sentences / sum_words
 
-print(sum_words)
+print(L)
