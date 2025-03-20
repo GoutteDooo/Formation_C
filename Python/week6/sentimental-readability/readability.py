bbe3474ca9f4ebb71f36846f7f7688193816aca5
GoutteDooo <goudal.donovan@live.fr>
2025-03-20 13:20:57 +0100

    2025-03-20 13:20:57

--- Python/week6/sentimental-readability/readability.py
+++ Python/week6/sentimental-readability/readability.py
@@ -1,7 +1,7 @@
 from cs50 import get_string
 
 inpute = "One Fish. Two Fish. Red Fish. Blue Fish."
-# inpute = "Would you like them here or there? I would not like them here or there. I would not like them anywhere."
+inpute = "Would you like them here or there? I would not like them here or there. I would not like them anywhere."
 # text = get_string("Text: ")
 text = inpute
 
@@ -16,12 +16,15 @@ L = 100 * sum_letters / len(words)
 
 # counts number of sentences
 sum_words = (len(words))
-sum_sentences = (len(text.split(".")))
 # get rid of blank items
-sum_sentences = [x for x in sum_sentences if x != 0]
+sum_sentences = len([x for x in text.split(".") if x != ""])
 S = 100 * (sum_sentences) / (sum_words)
 
-print(sum_sentences)
-print(L, S)
 ColemanLiauIndex = ((0.0588 * L) - (0.296 * S) - 15.8)
-print(round(ColemanLiauIndex))
+
+if ColemanLiauIndex < 0:
+  print("Before Grade 1")
+elif ColemanLiauIndex >= 0 and ColemanLiauIndex < 16:
+  print("Grade", round(ColemanLiauIndex))
+else:
+  print("Grade 16+")
