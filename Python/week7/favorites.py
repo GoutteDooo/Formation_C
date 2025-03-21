b0da9f34b289d458f26bebeac2b7af08d9031292
GoutteDooo <goudal.donovan@live.fr>
2025-03-21 21:30:46 +0100

    2025-03-21 21:30:46

--- Python/week7/favorites.py
+++ Python/week7/favorites.py
@@ -2,5 +2,15 @@ import csv
 
 with open("favorites.csv" ,"r") as file:
     reader =  csv.DictReader(file)
+    scratch, c, python = 0, 0, 0
     for row in reader:
-      print(row["tempo"])
+        favorite = row["language"]
+        if favorite == "Scratch":
+            scratch += 1
+        elif favorite == "C":
+            c += 1
+        elif favorite == "Python":
+            python += 1
+    print(f"Scratch: {scratch}")
+    print(f"C: {c}")
+    print(f"Python: {python}")
