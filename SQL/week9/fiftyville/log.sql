def86844a682201c550a3b961eaecb24a7d108f8
GoutteDooo <goudal.donovan@live.fr>
2025-03-30 15:58:01 +0200

    2025-03-30 15:58:01

--- SQL/week9/fiftyville/log.sql
+++ SQL/week9/fiftyville/log.sql
@@ -1,2 +1,9 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-SELECT * FROM crime_scene_reports;
+-- Obtain the description of the crime scene report that contains the word "CS50".
+SELECT * FROM crime_scene_reports WHERE description LIKE "%CS50 duck%";
+-- id   year  month  day  street           description                                                 
+-- ---  ----  -----  ---  ---------------  ------------------------------------------------------------
+-- 295  2024  7      28   Humphrey Street  Theft of the CS50 duck took place at 10:15am at the Humphrey
+--                                          Street bakery. Interviews were conducted today with three w
+--                                         itnesses who were present at the time – each of their interv
+--                                         iew transcripts mentions the bakery. 
