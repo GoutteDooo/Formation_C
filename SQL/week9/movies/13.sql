881e0a1d25e6df7299a200408c637b708006dec4
GoutteDooo <goudal.donovan@live.fr>
2025-03-30 10:29:08 +0200

    2025-03-30 10:29:08

--- SQL/week9/movies/13.sql
+++ SQL/week9/movies/13.sql
@@ -1,3 +1,9 @@
 SELECT * FROM stars WHERE person_id = (
   SELECT id FROM people WHERE name = "Kevin Bacon"
 );
+
+SELECT * FROM stars WHERE movie_id IN (
+  SELECT movie_id FROM stars WHERE person_id = (
+    SELECT id FROM people WHERE name = "Kevin Bacon"
+  )
+);
