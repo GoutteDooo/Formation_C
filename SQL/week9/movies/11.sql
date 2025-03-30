bec0ff48db17bf0a13b096ccbbdde2f90c4a9483
GoutteDooo <goudal.donovan@live.fr>
2025-03-30 10:03:33 +0200

    2025-03-30 10:03:33

--- SQL/week9/movies/11.sql
+++ SQL/week9/movies/11.sql
@@ -1,3 +1,3 @@
-SELECT * FROM movies JOIN ratings ON movie_id = movies.id WHERE movies.id IN (
-  SELECT movie_id FROM stars JOIN people ON person_id = id WHERE name = "Chadwick Boseman"
+SELECT * FROM movies JOIN ratings ON ratings.movie_id = movies.id WHERE movies.id IN (
+  SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE people.name = "Chadwick Boseman"
 );
