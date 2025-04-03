6604cad9b6a33ba78b494c9226b0ce7939d8f469
GoutteDooo <goudal.donovan@live.fr>
2025-04-03 17:21:35 +0200

    2025-04-03 17:21:35

--- html-css-js/homepage/script.js
+++ html-css-js/homepage/script.js
@@ -1,4 +1,15 @@
 const nameElement = document.getElementById("name-input");
+const player = {
+  name: "",
+  class: "",
+  gold: 0,
+  stats: {
+    level: 0,
+    strength: 0,
+    defense: 0,
+    health : 0
+  }
+}
 
 const enterName = (e) => {
   nameElement.textContent = `Your name is: ${e.target.value}`;
