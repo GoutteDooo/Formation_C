25e7713a3ffe94ad94acd1b5fced47a88da39194
GoutteDooo <goudal.donovan@live.fr>
2025-04-04 16:23:38 +0200

    2025-04-04 16:23:38

--- html-css-js/homepage/boss.js
+++ html-css-js/homepage/boss.js
@@ -9,7 +9,8 @@ const boss = {
     "exp": [10000,10000],
     "distance": [10000,10000],
     "probability": 10,
-    "victory_msg": "You're a hero. You saved your village from the claws of Zrog the Dragon."
+    "victory_msg": "You're a hero. You saved your village from the claws of Zrog the Dragon.",
+    "defeat_msg": "You've never taken such a beating in your life. "
   }
 }
 
--- html-css-js/homepage/village.js
+++ html-css-js/homepage/village.js
@@ -153,7 +153,8 @@ function rest(e) {
 //State 2 : BOSS
 
 function fightBoss(e) {
-  console.log("fight boss!");
+  const boss_object = boss.boss1;
+  fight(boss_object);
 }
 
 /* ACTIONS END */
@@ -280,7 +281,14 @@ const fight = (monster) => {
   if (player_object.stats.health <= 0) {
     player_object.stats.health = 1;
     const game_datas = JSON.parse(localStorage.getItem("game_datas"));
-    game_datas.player_infos = `You fought against a ${monster_object.name} and lost. <br /> Your body has been reconstitued thanks to the village altar, but it is weak now. <br /> You have to regain lives before exploring the world again.`;
+    game_datas.player_infos = 
+    `You fought against a ${monster_object.name} and lost.
+      <br />
+     <span class="text-secondary">${monster_object.defeat_msg ? monster_object.defeat_msg : ""}</span>
+     <br /> 
+     Your body has been reconstitued thanks to the village altar, but it is weak now. 
+     <br /> 
+     You have to regain lives before exploring the world again.`;
     localStorage.setItem("game_datas", JSON.stringify(game_datas));
     //update player datas
     localStorage.setItem("player", JSON.stringify(player_object));
