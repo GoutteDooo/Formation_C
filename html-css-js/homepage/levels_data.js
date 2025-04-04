673d441f45570ee8b3415846e0ba89c0ebea5b9a
GoutteDooo <goudal.donovan@live.fr>
2025-04-04 11:26:15 +0200

    2025-04-04 11:26:15

--- html-css-js/homepage/levels_data.js
+++ html-css-js/homepage/levels_data.js
@@ -1,21 +1,74 @@
 const levels_data = {
   "1": {
     "exp_needed":200,
-    "new_stats": {
+    "stats_upgrade": {
+      "Warrior": {
+        "strength": 2,
+        "mental": 0,
+        "max_health": 30,
+      }
+    }
+  },
+  "2": {
+    "exp_needed":500,
+    "stats_upgrade": {
+      "Warrior": {
+        "strength": 2,
+        "mental": 0,
+        "max_health": 50,
+      }
+    }
+  },
+  "3": {
+    "exp_needed":1000,
+    "stats_upgrade": {
       "Warrior": {
         "strength": 3,
         "mental": 0,
-        "health": 30,
-        "max_health": 30,
-        "exp": 0,
+        "max_health": 100,
       }
     }
-  "2": 500,
-  "3": 1000,
-  "4": 1700,
-  "5": 2900,
-  "6": 4800,
-  "7": 7777,
+  },
+  "4": {
+    "exp_needed":1700,
+    "stats_upgrade": {
+      "Warrior": {
+        "strength": 3,
+        "mental": 1,
+        "max_health": 170,
+      }
+    }
+  },
+  "5": {
+    "exp_needed":2900,
+    "stats_upgrade": {
+      "Warrior": {
+        "strength": 3,
+        "mental": 1,
+        "max_health": 200,
+      }
+    }
+  },
+  "6": {
+    "exp_needed":4800,
+    "stats_upgrade": {
+      "Warrior": {
+        "strength": 4,
+        "mental": 1,
+        "max_health": 250,
+      }
+    }
+  },
+  "7": {
+    "exp_needed":7777,
+    "stats_upgrade": {
+      "Warrior": {
+        "strength": 5,
+        "mental": 2,
+        "max_health": 350,
+      }
+    }
+  },
 }
 
 
