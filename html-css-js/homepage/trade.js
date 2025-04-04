395262d4ac1674265d3f522aae5e597a1243a982
GoutteDooo <goudal.donovan@live.fr>
2025-04-04 17:40:41 +0200

    2025-04-04 17:40:40

--- html-css-js/homepage/trade.js
+++ html-css-js/homepage/trade.js
@@ -1,5 +1,16 @@
 const internet = document.getElementById("internet");
+const returnBtn = document.getElementById("return");
+
+document.addEventListener("DOMContentLoaded", () => {
+  returnBtn.addEventListener("mouseup", () => {
+    window.location.href = "village.html";
+  });
+  updatePage();
+});
+
+
 
 const updatePage = () => {
-  
+
 }
+
