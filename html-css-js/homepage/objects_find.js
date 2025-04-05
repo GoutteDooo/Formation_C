4fa2c1790df007b006ea4f9f1b6cd937ed3a34c9
GoutteDooo <goudal.donovan@live.fr>
2025-04-05 14:19:38 +0200

    2025-04-05 14:19:38

--- html-css-js/homepage/objects_find.js
+++ html-css-js/homepage/objects_find.js
@@ -1,12 +1,52 @@
-{
-  "diamond": 0.01,
-  "gold ring": 0.03,
-  "pretty watch": 0.05,
-  "straw hat with red ribbon": 0.08,
-  "smelly sock": 0.10,
-  "used golf club": 0.10,
-  "old straw hat": 0.12,
-  "plastic laser sword": 0.15,
-  "old book": 0.16,
-  "screen broken iPhone": 0.20
+const find_object = {
+  object1: {
+    "name":"diamond",
+    "probability": 0.01,
+    "price": 15000,
+  },
+  object2: {
+    "name":"gold ring",
+    "probability": 0.03,
+    "price": 6000,
+  },
+  object3: {
+    "name":"pretty watch",
+    "probability": 0.05,
+    "price": 1800,
+  },
+  object4: {
+    "name":"straw hat with red ribbon",
+    "probability": 0.08,
+    "price": 550,
+  },
+  object5: {
+    "name":"smelly sock",
+    "probability": 0.10,
+    "price": 300,
+  },
+  object6: {
+    "name":"used golf club",
+    "probability": 0.10,
+    "price": 300,
+  },
+  object7: {
+    "name":"old straw hat",
+    "probability": 0.12,
+    "price": 150,
+  },
+  object8: {
+    "name":"old book",
+    "probability": 0.16,
+    "price": 100,
+  },
+  object9: {
+    "name":"plastic laser sword",
+    "probability": 0.15,
+    "price": 50,
+  },
+  object10: {
+    "name":"screen broken iPhone",
+    "probability": 0.20,
+    "price": 50,
+  },
 }
