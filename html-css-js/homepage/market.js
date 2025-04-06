6b62b1b6f5ca73b60a57910ea23eb50956d7302c
GoutteDooo <goudal.donovan@live.fr>
2025-04-06 12:45:15 +0200

    2025-04-06 12:45:15

--- html-css-js/homepage/market.js
+++ html-css-js/homepage/market.js
@@ -14,8 +14,30 @@ const market = {
         default_price_range: [1100,1500],
         lowest_selling_price: 1000
       }
+    ],
+    "buying_objects": [
+      {
+        id:4,
+        default_buying_range: []
+      }
+  },
+  seller2: {
+    "name": "Jean12345",
+    "nuts": 10000,
+    "description": "He is hard in business.",
+    "selling_objects": [
+      {
+        id:9,
+        default_price_range: [520,600],
+        lowest_selling_price: 455,
+      },
+      {
+        id:13,
+        default_price_range: [1100,1500],
+        lowest_selling_price: 1000
+      }
     ]
-  }
+  },
 }
 
 export default market;
