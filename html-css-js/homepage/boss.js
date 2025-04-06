e05b2f4f6e71b571f16fea2ea4dd0425d45ef4d2
GoutteDooo <goudal.donovan@live.fr>
2025-04-06 12:06:44 +0200

    2025-04-06 12:06:44

--- html-css-js/homepage/boss.js
+++ html-css-js/homepage/boss.js
@@ -5,7 +5,7 @@ const boss = {
     "strength": 25,
     "mental": 25,
     "default_attack": "all",
-    "gold": [10000,10000],
+    "nuts": [10000,10000],
     "exp": [10000,10000],
     "distance": [10000,10000],
     "victory_msg": "You're a hero. You saved your village from the claws of Zrog the Dragon.",
