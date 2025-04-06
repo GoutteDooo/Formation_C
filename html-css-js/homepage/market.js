4bd418f88549859cb5ab1da7d18ff748e48f9676
GoutteDooo <goudal.donovan@live.fr>
2025-04-06 12:39:15 +0200

    2025-04-06 12:39:15

--- html-css-js/homepage/market.js
+++ html-css-js/homepage/market.js
@@ -1,5 +1,14 @@
 const market = {
-  
+  seller1: {
+    "name": "Mr. Smith",
+    "nuts": 10000,
+    "selling_objects": [
+      {
+        id:9,
+        default_price: 
+        buying_range: []
+      }
+  }
 }
 
 export default market;
