5ec3584d5ebd7fa708d46400e3bd6bad67acdfac
GoutteDooo <goudal.donovan@live.fr>
2025-04-07 15:58:16 +0200

    2025-04-07 15:58:16

--- html-css-js/homepage/victory.js
+++ html-css-js/homepage/victory.js
@@ -1,11 +1,36 @@
 function animateText() {
-  const text = document.querySelector("h1");
-  let selector = text.textContent[0];
-  console.log(selector);
-  
+  const text = document.querySelector("h1").textContent;
+  let index_text = 0;
+  const colors = [
+    "#00ff00",
+    "#ff0000",
+    "#0000ff",
+    "#ffff00",
+    "#ff00ff",
+    "#00ffff",
+    "#000000",
+    "#ffffff",
+    "#ff0000",
+    "#00ff00",
+    "#0000ff",
+  ]
   setInterval(() => {
-    text.innerHTML = "YOU WIN !!!";
+    text[index_text] = "a";
+    index_text++;
   }, 100);
 }
 
-animateText();
+function removeFadeOut() {
+  const FADE_OUT = document.querySelector("#fade-out");
+
+  setTimeout(() => {
+    FADE_OUT.remove();
+  }, 3000);
+}
+
+function initVictory() {
+  removeFadeOut();
+  animateText();
+}
+
+initVictory();
