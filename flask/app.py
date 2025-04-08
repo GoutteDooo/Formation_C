c87689dc603b2467e45ffe5fa86a81490c5532aa
GoutteDooo <goudal.donovan@live.fr>
2025-04-08 11:03:11 +0200

    2025-04-08 11:03:11

--- flask/app.py
+++ flask/app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 @app.route('/', methods=["GET","POST"])
 def index():
   if request.method == "POST":
-    name = request.form.get("name")
-    return render_template('greet.html', name=name)
+    return render_template('greet.html', name=request.form.get('name'))
   else:
     return render_template('index.html')
