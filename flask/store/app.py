6ff338c1e7d4f74face6f1de5d38e2202ff9c9bb
GoutteDooo <goudal.donovan@live.fr>
2025-04-08 18:15:37 +0200

    2025-04-08 18:15:37

---
+++ flask/store/app.py
@@ -0,0 +1,16 @@
+from cs50 import SQL
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///store.db")
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route('/')
+def index():
+  books = db.execute("SELECT * FROM books")
+  return render_template("books.html", books=books)
