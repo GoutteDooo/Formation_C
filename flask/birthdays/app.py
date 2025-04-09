b647415c3ae2635fd2f3139e74d3e0d4e79946b1
GoutteDooo <goudal.donovan@live.fr>
2025-04-09 10:38:25 +0200

    2025-04-09 10:38:25

--- flask/birthdays/app.py
+++ flask/birthdays/app.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET","POST"])
 def index():
-  friend = request.form.get("friend")
+  friend_name = request.form.get("friend")
+  if not friend_name:
+    return redirect("/")
   month = request.form.get("month")
   day = request.form.get("day")
   if request.method == "POST":
-    db.execute("INSERT INTO birthdays (friend, month, day) VALUES (?, ?, ?)", friend, month, day)
+    db.execute("INSERT INTO birthdays (friend, month, day) VALUES (?, ?, ?)", friend_name, month, day)
     return redirect("/")
   return render_template("index.html")
