72d18b4f21a53c7564e2c05db09a8618ea4b6838
GoutteDooo <goudal.donovan@live.fr>
2025-04-09 10:13:54 +0200

    2025-04-09 10:13:54

--- flask/birthdays/app.py
+++ flask/birthdays/app.py
@@ -5,4 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+  friend = request.form.get("friend")
+  month = request.form.get("month")
+  day = request.form.get("day")
   return render_template("index.html")
